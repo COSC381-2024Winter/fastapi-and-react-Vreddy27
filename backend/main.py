d08a94a804c816e6e6cc34bb8e76d32c34c3fbfa
Vidisha Reddy pallamreddy <vpallamr@emich.edu>
2024-04-16 14:47:52 +0000

    Feature: PUT method to update movie information
    1. Implemented a put function which can be used to change movie title and cast by using the movie Id
    2. Made changes to main.py, added the PUT method and movies request class.
    
    Issue #3

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 
 movies = Movies('./movies.txt')
 
+class MovieRequest(BaseModel):
+    movie_name: str
+    movie_cast: list[str]
+
 @app.get("/")
 def welcome():
     return "Hello World"
@@ -16,5 +20,15 @@ def welcome():
 def find_movie_by_id(movie_id: int) -> Union[Movie, None]:
     for movie in movies._movies:
         if movie['id'] == movie_id:
+            return movie
+    raise HTTPException(status_code=404, detail="Movie not found")
+
+
+@app.put("/movies/{movie_id}", response_model=Movie)
+def update_movie(movie_id: int, new_movie: MovieRequest) -> Union[Movie, None]:
+    for movie in movies._movies:
+        if movie['id'] == movie_id:
+            movie['name'] = new_movie.movie_name
+            movie['cast'] = new_movie.movie_cast
             return movie
     raise HTTPException(status_code=404, detail="Movie not found")
